06659a28b7c6b3b52e8e414b0973754a90dad602
Gabi\Gabriel <gdincagabriel@gmail.com>
2018-03-27 21:37:00 +0300

    Added feature for calculating the sum of the vector's elements using MPI_Reduce

--- MPI_Sum_of_Vector_Elements/MPI_Sum_of_Vector_Elements/main.cpp
+++ MPI_Sum_of_Vector_Elements/MPI_Sum_of_Vector_Elements/main.cpp
@@ -1,28 +1,42 @@
 #include <mpi.h>
 #include <stdio.h>
 
+#define NUMBER_OF_ELEMENTS 16
+#define MASTER 0
+
 int main(int argc, char** argv) {
-	// Initialize the MPI environment
+
+	int v[NUMBER_OF_ELEMENTS], numprocs, procid, len, partner, message;
+	char hostname[MPI_MAX_PROCESSOR_NAME];
+	for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+	{
+		v[i] = i;
+	}
+
 	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &procid);
+	MPI_Get_processor_name(hostname, &len);
 
-	// Get the number of processes
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	int step = NUMBER_OF_ELEMENTS / numprocs;
 
-	// Get the rank of the process
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	int partial_sum = 0;
+	printf("Processor: %d, Elements are: ", procid);
+	for(int i = procid * step ; i < procid * step + step; i++)
+	{	
+		partial_sum += v[i];
+		printf(" %d ", v[i]);
+	}
+	printf("-- Sum of the elements is: %d \n", partial_sum);
 
-	// Get the name of the processor
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
+	int total_sum;
+	MPI_Reduce(&partial_sum, &total_sum, 1, MPI_INT, MPI_SUM, MASTER, MPI_COMM_WORLD);
 
-	// Print off a hello world message
-	printf("Hello world from processor %s, rank %d"
-		" out of %d processors\n",
-		processor_name, world_rank, world_size);
+	if (procid == MASTER)
+	{
+		printf("MASTER: The total sum of the arrays's elements is: %d \n", total_sum);
+	}
 
-	// Finalize the MPI environment.
 	MPI_Finalize();
+
 }
